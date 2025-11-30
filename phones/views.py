559fab4e650eaf925d6a20345897cbc973291896
VladPrickly <vlad.prickly@gmail.com>
2025-12-01 02:07:36 +0300

    add sorting

--- phones/views.py
+++ phones/views.py
@@ -1,4 +1,3 @@
-
 from django.shortcuts import render, redirect
 
 from phones.models import Phone
@@ -10,14 +9,30 @@ def index(request):
 
 def show_catalog(request):
     template = 'catalog.html'
-    phones = Phone.objects.all()
-    context = {'phones': phones}
+    sort_pages = request.GET.get('sort', '')
+    all_phones = Phone.objects.all()
+
+    if sort_pages == 'name':
+        phone = all_phones.order_by('name')
+        context = {'phones': phone}
+        return render(request, template, context)
+
+    elif sort_pages == 'min_price':
+        phone = all_phones.order_by('price')
+        context = {'phones': phone}
+        return render(request, template, context)
+
+    elif sort_pages == 'max_price':
+        phone = all_phones.order_by('-price')
+        context = {'phones': phone}
+        return render(request, template, context)
+
+    context = {'phones': all_phones}
     return render(request, template, context)
 
 
 def show_product(request, slug):
     template = 'product.html'
     phone = Phone.objects.get(slug=slug)
-    context = {'phones':phone}
+    context = {'phones': phone}
     return render(request, template, context)
-
